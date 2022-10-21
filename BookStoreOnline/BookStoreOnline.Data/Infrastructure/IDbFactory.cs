fa52db077be637a06deb1569344cd720ce083733
lenguyenductamht <lenguyenductamht@gmail.com>
2022-10-21 13:22:39 +0700

    delete all file

--- BookStoreOnline/BookStoreOnline.Data/Infrastructure/IDbFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace BookStoreOnline.Data.Infrastructure
-{
-    public interface IDbFactory : IDisposable
-    {
-        BookStoreOnlineDbContext Init();
-    }
-}
--- BookStoreOnline/BookStoreOnline.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookStoreOnline.Web.MvcApplication" Language="C#" %>
